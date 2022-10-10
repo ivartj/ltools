33eded7cb992d9b2da87a95749c3a6812a308b9e
Ivar Trygve Jarlsby <ivartj@ivartj.org>
2022-10-10 08:10:58 +0200

    Adjustments to crstrip.rs after exercise.

--- src/crstrip.rs
+++ src/crstrip.rs
@@ -23,41 +23,42 @@ impl<W: Write> Write for CrStripper<W> {
         let mut write_from = 0;
         for (i, c) in buf.iter().copied().enumerate() {
             self.state = match self.state {
-                State::Normal => {
-                    match c {
-                        b'\r' => {
-                            if i != 0 {
-                                self.inner.write(&buf[write_from..i])?;
-                            }
-                            write_from = i + 1;
-                            State::Cr
-                        },
-                        _ => State::Normal,
-                    }
+                State::Normal => match c {
+                    b'\r' => State::Cr,
+                    _ => State::Normal,
                 },
                 State::Cr => {
-                    if c != b'\n' {
-                        self.inner.write(b"\r")?;
-                    }
-                    if c == b'\r' {
-                        write_from = i + 1;
-                        State::Cr
-                    } else {
+                    if c == b'\n' {
+                        if i > 1 {
+                            self.inner.write(&buf[write_from..i-1])?;
+                        }
+                        write_from = i;
                         State::Normal
+                    } else {
+                        if i == 0 {
+                            self.inner.write(b"\r")?;
+                        }
+                        match c {
+                            b'\r' => State::Cr,
+                            _ => State::Normal,
+                        }
                     }
                 },
             };
         }
+        let write_until = if self.state == State::Cr {
+            buf.len() - 1
+        } else {
+            buf.len()
+        };
 
-        if write_from < buf.len() {
-            self.inner.write(&buf[write_from..])?;
-        }
+        self.inner.write(&buf[write_from..write_until])?;
 
         Ok(buf.len())
     }
 
     fn flush(&mut self) -> Result<()> {
-        Ok(())
+        todo!()
     }
 }
 
@@ -70,7 +71,7 @@ mod test {
         let mut buf = Vec::new();
         let mut crstripper = CrStripper::new(&mut buf);
         crstripper.write(b"foo\r\nbar")?;
-        assert_eq!(buf.as_slice(), b"foo\nbar");
+        assert_eq!(String::from_utf8_lossy(buf.as_slice()), "foo\nbar");
         Ok(())
     }
 
@@ -80,7 +81,7 @@ mod test {
         let mut crstripper = CrStripper::new(&mut buf);
         crstripper.write(b"foo\r")?;
         crstripper.write(b"\nbar")?;
-        assert_eq!(buf.as_slice(), b"foo\nbar");
+        assert_eq!(String::from_utf8_lossy(buf.as_slice()), "foo\nbar");
         Ok(())
     }
 
